43dbae77de24d59a512c68791da535b388ba0230
gabriel-skinny <gabrielcatoni12@gmail.com>
2024-08-29 09:45:59 -0300

    Creating database mappers

--- src/infra/database/mappers/url.ts
+++ src/infra/database/mappers/url.ts
@@ -1,14 +1,29 @@
 import { Url } from "src/application/entities/Url";
 import { UrlEntity } from "../entities/url";
 
-export const urlEntityToRaw = (url: UrlEntity) => {
-    return new Url({
-        id: url.id,
-        destinyUrl: url.destinyUrl,
-        userId: url.userId,
-        clickNumber: url.clickNumber,
-        createdAt: url.createdAt,
-        deletedAt: url.deletedAt,
-        updatedAt: url.updatedAt,
-    })
+export class UrlEntityMapper {
+    static toDomain(url: UrlEntity): Url {
+        return new Url({
+            id: url.id,
+            destinyUrl: url.destinyUrl,
+            userId: url.userId,
+            clickNumber: url.clickNumber,
+            createdAt: url.createdAt,
+            deletedAt: url.deletedAt,
+            updatedAt: url.updatedAt,
+        })
+    }
+
+    static toDatabase(url: Url): UrlEntity {
+        return {
+            id: url.id,
+            destinyUrl: url.destinyUrl,
+            userId: url.userId,
+            clickNumber: url.clickNumber,
+            shortenedUrl: url.shortenedUrl,
+            createdAt: url.createdAt,
+            deletedAt: url.deletedAt,
+            updatedAt: url.updatedAt,
+        }
+    }
 }
