473f71d3050f5e0f351e4341ddafa1be07c543ba
gabriel-skinny <gabrielcatoni12@gmail.com>
2024-08-29 18:29:57 -0300

    Creating decoretor to make conditional authroization

---
+++ src/infra/http/decoretors/conditionalGuard.ts
@@ -0,0 +1,3 @@
+import { Reflector } from '@nestjs/core';
+
+export const ConditionalGuardByField = Reflector.createDecorator<string>();
---
+++ src/infra/http/guards/Autentication.ts
@@ -0,0 +1,55 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Request } from 'express';
+import { AbstractAuthService } from 'src/application/services/jwt';
+import { ConditionalGuardByField } from '../decoretors/conditionalGuard';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private authService: AbstractAuthService,
+    private reflector: Reflector,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+
+    const conditionalGuardField = this.reflector.get(
+      ConditionalGuardByField,
+      context.getHandler(),
+    );
+
+    if (conditionalGuardField && !request.body[conditionalGuardField])
+      return true;
+
+    const token = this.extractTokenFromHeader(request);
+    if (!token) throw new UnauthorizedException();
+
+    try {
+      const payload = await this.authService.verifyToken(token);
+
+      if (
+        conditionalGuardField &&
+        payload.userId !== request.body[conditionalGuardField]
+      )
+        throw new UnauthorizedException(
+          'Value passed does not match the token value',
+        );
+
+      request['user'] = payload;
+    } catch {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
