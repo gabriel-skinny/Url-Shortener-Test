4ae1f6055ccfb1332a0822d469f206c90887e2e5
gabriel-skinny <gabrielcatoni12@gmail.com>
2024-08-29 11:31:59 -0300

    finishing url-shortener controller

--- src/infra/http/controllers/url-shortener.ts
+++ src/infra/http/controllers/url-shortener.ts
@@ -1,19 +1,97 @@
-import { Controller, Delete, Get, Patch, Post } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Res,
+} from '@nestjs/common';
+import { Response } from 'express';
+import { CreateShortUrlUseCase } from 'src/application/use-cases/url-shortener/create';
+import { DeleteUrlUseCase } from 'src/application/use-cases/url-shortener/delete';
+import { GetUrlsByUserIdUseCase } from 'src/application/use-cases/url-shortener/get-urls-by-user-id';
+import { RedirectUseCase } from 'src/application/use-cases/url-shortener/redirect';
+import { UpdateUrlUseCase } from 'src/application/use-cases/url-shortener/update';
+import { BaseControllerReturn } from 'src/infra/interfaces/baseController';
+import { CreateUrlDTO, DeleteUrlDTO, UpdateUrlDTO } from '../dto/url';
+import { IUrlViewModel, UrlViewModel } from '../view-models/url';
 
-@Controller("url")
-export class UserController {
-    @Post()
-    async create() { }
+@Controller('url')
+export class UrlController {
+  constructor(
+    private readonly createUrlUseCase: CreateShortUrlUseCase,
+    private readonly deleteUrlUseCase: DeleteUrlUseCase,
+    private readonly getUrlsByUserIdUseCase: GetUrlsByUserIdUseCase,
+    private readonly redirectUseCase: RedirectUseCase,
+    private readonly updateUseCase: UpdateUrlUseCase,
+  ) {}
 
-    @Post()
-    async redirect() { }
+  @Post()
+  async create(
+    @Body() { destinyUrl, userId }: CreateUrlDTO,
+  ): Promise<BaseControllerReturn<{ shortednedUrl: string }>> {
+    const { shortednedUrl } = await this.createUrlUseCase.execute({
+      destinyUrl,
+      userId,
+    });
 
-    @Delete()
-    async delete() { }
+    return {
+      message: 'Url created',
+      data: { shortednedUrl },
+      statusCode: HttpStatus.CREATED,
+    };
+  }
 
-    @Patch()
-    async update() { }
+  @Post(':urlValue')
+  async redirect(@Param('urlValue') urlValue: string, @Res() res: Response) {
+    const { desitinyUrl } = await this.redirectUseCase.execute({
+      shortenedUrl: urlValue,
+    });
 
-    @Get()
-    async getUrlsByUserId() { }
+    res.redirect(desitinyUrl);
+  }
+
+  @Delete()
+  async delete(
+    @Body() { urlId, userId }: DeleteUrlDTO,
+  ): Promise<BaseControllerReturn> {
+    await this.deleteUrlUseCase.execute({ urlId, userId });
+
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Url deleted sucessfully',
+    };
+  }
+
+  @Patch()
+  async update(
+    @Body() { newDestinyUrl, urlId, userId }: UpdateUrlDTO,
+  ): Promise<BaseControllerReturn> {
+    await this.updateUseCase.execute({
+      newDestinyUrl,
+      urlId,
+      userId,
+    });
+
+    return {
+      message: 'Url updated sucessfully',
+      statusCode: HttpStatus.OK,
+    };
+  }
+
+  @Get('many/:id')
+  async getUrlsByUserId(
+    @Param('id') id: string,
+  ): Promise<BaseControllerReturn<{ urls: IUrlViewModel[] }>> {
+    const urls = await this.getUrlsByUserIdUseCase.execute({ userId: id });
+
+    return {
+      message: 'User urls',
+      statusCode: HttpStatus.OK,
+      data: { urls: urls.map(UrlViewModel.toHttp) },
+    };
+  }
 }
