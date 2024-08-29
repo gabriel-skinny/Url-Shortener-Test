2828aec02754103f073bdf9d1af221501607804d
gabriel-skinny <gabrielcatoni12@gmail.com>
2024-08-29 07:20:06 -0300

    Finishing get urls by user id test

---
+++ src/application/use-cases/tests/get-urls-by-user-id.spec.ts
@@ -0,0 +1,24 @@
+import { InMemoryShortUrlRepository } from "src/application/repositories/inMemoryShortUrlRepository";
+import { GetUrlsByUserId } from "../get-urls-by-user-id"
+import { makeShortUrl } from "./factories/makeShortUrl";
+
+const makeSut = () => {
+    const urlRepository = new InMemoryShortUrlRepository();
+    const getUrlsByUserId = new GetUrlsByUserId(urlRepository);
+
+    return { urlRepository, getUrlsByUserId }
+}
+
+describe("Get urls by user id use case", () => {
+    it("Should return all short urls of a user", async () => {
+        const { getUrlsByUserId, urlRepository } = makeSut();
+
+        const userId = "userId123";
+        const shortUrlsCreated = [makeShortUrl({ userId }), makeShortUrl({ userId }), makeShortUrl({ userId })];
+        for (const shortUrl of shortUrlsCreated) await urlRepository.save(shortUrl);
+
+        const response = await getUrlsByUserId.execute({ userId });
+
+        expect(response).toStrictEqual(shortUrlsCreated);
+    })
+})
