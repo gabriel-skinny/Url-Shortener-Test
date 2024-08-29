2a542ed76f777ecb1c2426fa026ba40139fc7b04
gabriel-skinny <gabrielcatoni12@gmail.com>
2024-08-29 09:48:28 -0300

    Adding new methods

--- src/application/repositories/inMemoryShortUrlRepository.ts
+++ src/application/repositories/inMemoryShortUrlRepository.ts
@@ -1,16 +1,19 @@
 import { Url } from "../entities/Url";
-import { IShortUrlRepository } from "./shortUrlRepository";
+import { AbstractShortUrlRepository } from "./shortUrlRepository";
 
-export class InMemoryShortUrlRepository implements IShortUrlRepository {
-
+export class InMemoryShortUrlRepository implements AbstractShortUrlRepository {
     public shortUrlDatabase: Url[] = [];
 
     async save(data: Url): Promise<void> {
         this.shortUrlDatabase.push(data);
     }
 
-    async findByUrlDesitinyAndUser(data: { destinyUrl: string; userId?: string; }): Promise<Url | null> {
+    async findByUrlDestinyUrlAndUser(data: { destinyUrl: string; userId?: string; }): Promise<Url | null> {
         return this.shortUrlDatabase.find(url => url.destinyUrl == data.destinyUrl && url.userId == data.userId);
+    }
+
+    async findByUrlDestinyUrl(data: { destinyUrl: string; }): Promise<Url | null> {
+        return this.shortUrlDatabase.find(url => url.destinyUrl == data.destinyUrl);
     }
 
     async findByIdAndUserId(data: { userId: string; id: string; }): Promise<Url | null> {
