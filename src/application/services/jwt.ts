224f100697dd4bbfd17375b1bbab03adc6d2e82f
gabriel-skinny <gabrielcatoni12@gmail.com>
2024-08-29 18:32:09 -0300

    Changing interface to abstract class

--- src/application/services/jwt.ts
+++ src/application/services/jwt.ts
@@ -1,3 +1,10 @@
-export interface IJwtService {
-  generateToken(data: Record<any, any>): Promise<{ token: string }>;
+import { ILoginTokenData } from 'src/infra/services/Auth';
+
+export abstract class AbstractAuthService {
+  abstract generateLoginToken(data: {
+    userId: string;
+    email: string;
+    name: string;
+  }): Promise<{ token: string }>;
+  abstract verifyToken(token: string): Promise<ILoginTokenData>;
 }
--- src/application/services/jwtMock.ts
+++ src/application/services/jwtMock.ts
@@ -1,7 +1,15 @@
-import { IJwtService } from './jwt';
+import { ILoginTokenData } from 'src/infra/services/Auth';
+import { AbstractAuthService } from './jwt';
 
-export class JwtMock implements IJwtService {
-  async generateToken(data: Record<any, any>): Promise<{ token: string }> {
+export class JwtMock implements AbstractAuthService {
+  verifyToken(token: string): Promise<ILoginTokenData> {
+    throw new Error('Method not implemented.');
+  }
+  async generateLoginToken(data: {
+    userId: string;
+    email: string;
+    name: string;
+  }): Promise<{ token: string }> {
     return { token: 'mockedToken' };
   }
 }
