77e40e68c1e830c22bd4fb1b66c5db56f0eef2cc
gabriel-skinny <gabrielcatoni12@gmail.com>
2024-08-29 18:33:24 -0300

    Adding new modules to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseModule } from './infra/database/database.module';
+import { HttpModule } from './infra/http/http.module';
+import { ServiceModule } from './infra/services/services.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, HttpModule, ServiceModule],
   controllers: [],
   providers: [],
 })
