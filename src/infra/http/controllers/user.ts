8ee2812c87a83c7fec75bef36be35796ae0bd551
gabriel-skinny <gabrielcatoni12@gmail.com>
2024-08-29 11:32:12 -0300

    Finishing user controller

--- src/infra/http/controllers/user.ts
+++ src/infra/http/controllers/user.ts
@@ -1,10 +1,45 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
+import { CreateUserUseCase } from 'src/application/use-cases/user/create';
+import { LoginUseCase } from 'src/application/use-cases/user/login';
+import { BaseControllerReturn } from 'src/infra/interfaces/baseController';
+import { CreateUserDTO, LoginDTO } from '../dto/user';
 
-@Controller("user")
+@Controller('user')
 export class UserController {
-    @Post()
-    async create() { }
+  constructor(
+    private createUserUseCase: CreateUserUseCase,
+    private loginUseCase: LoginUseCase,
+  ) {}
 
-    @Post()
-    async login() { }
+  @Post()
+  async create(
+    @Body() { email, name, password }: CreateUserDTO,
+  ): Promise<BaseControllerReturn> {
+    await this.createUserUseCase.execute({
+      email,
+      name,
+      password,
+    });
+
+    return {
+      message: 'User created succesfully',
+      statusCode: HttpStatus.OK,
+    };
+  }
+
+  @Post()
+  async login(
+    @Body() { email, password }: LoginDTO,
+  ): Promise<BaseControllerReturn<{ token: string }>> {
+    const { token } = await this.loginUseCase.execute({
+      email,
+      password,
+    });
+
+    return {
+      message: 'loged in succesfully',
+      statusCode: HttpStatus.OK,
+      data: { token },
+    };
+  }
 }
