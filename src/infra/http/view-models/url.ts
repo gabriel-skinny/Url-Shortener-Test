72e8b6c8f9559aec47dc6ee6e1c3c2408c4aedc9
gabriel-skinny <gabrielcatoni12@gmail.com>
2024-08-29 11:31:36 -0300

    Creating view-models

---
+++ src/infra/http/view-models/url.ts
@@ -0,0 +1,23 @@
+import { Url } from "src/application/entities/Url";
+
+export interface IUrlViewModel {
+    id: string;
+    destinyUrl: string;
+    userId?: string;
+    clickNumber: number;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export class UrlViewModel {
+    static toHttp(url: Url): IUrlViewModel {
+        return {
+            id: url.id,
+            destinyUrl: url.id,
+            userId: url.id,
+            clickNumber: url.clickNumber,
+            createdAt: url.createdAt,
+            updatedAt: url.updatedAt,
+        }
+    }
+}
