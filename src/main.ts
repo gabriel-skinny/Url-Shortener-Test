9df88802f7b773ed9d534b9bf6e67906184273ef
gabriel-skinny <gabrielcatoni12@gmail.com>
2024-08-29 18:39:48 -0300

    Adding swagger

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,22 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import 'dotenv/config';
 import { CustomExceptionFilter } from './infra/http/filters/httpException';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new CustomExceptionFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('Encurtador de Url')
+    .setDescription('Uma api que encurta qualquer url')
+    .setVersion('1.0')
+    .addTag('urlShortener')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
