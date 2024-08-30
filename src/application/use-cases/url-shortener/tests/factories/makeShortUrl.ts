ac9d13a62537befe5bd40c905726341c374c87c1
gabriel-skinny <gabrielcatoni12@gmail.com>
2024-08-29 21:29:12 -0300

    Removing non used imports

--- src/application/use-cases/url-shortener/tests/factories/makeShortUrl.ts
+++ src/application/use-cases/url-shortener/tests/factories/makeShortUrl.ts
@@ -1,5 +1,4 @@
 import { IUrlProps, Url } from 'src/application/entities/Url';
-import { IUserProps, User } from 'src/application/entities/User';
 
 export const makeShortUrl = (props?: Partial<IUrlProps>) => {
   return new Url({
