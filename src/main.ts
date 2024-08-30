d96c5cf7a763eb844aa5e72178f69ec04e464c0b
gabriel-skinny <gabrielcatoni12@gmail.com>
2024-08-30 19:02:34 -0300

    Adding helmet

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,15 @@ import { ValidationPipe } from '@nestjs/common';
 import 'dotenv/config';
 import { CustomExceptionFilter } from './infra/http/filters/httpException';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new CustomExceptionFilter());
+
+  app.use(helmet());
 
   const config = new DocumentBuilder()
     .setTitle('Encurtador de Url')
